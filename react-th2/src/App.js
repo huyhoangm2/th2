75ccaff9634ca0e83e76d1a5de3677716ffdb397
huyhoangm2 <hoangken760@gmail.com>
2023-02-03 21:05:16 +0700

    fcm

---
+++ react-th2/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <div>Đây là content</div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
